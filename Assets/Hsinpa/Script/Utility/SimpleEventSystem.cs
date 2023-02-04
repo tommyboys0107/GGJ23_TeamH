a5d02d30a533a1fbe5958f40b2028d0272d7ae1e
hsinpa <hsinpa@gmail.com>
2023-02-04 17:42:36 +0800

    Upload simple event system

---
+++ Assets/Hsinpa/Script/Utility/SimpleEventSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hsinpa.Utility {
+    public static class SimpleEventSystem
+    {
+        public static System.Action<int, object[]> CustomEventListener;
+        public static System.Action OnDisposeEvent;
+
+        public static void Send(int tag, params object[] customObjects) {
+            try
+            {
+                if (CustomEventListener != null)
+                {
+                    CustomEventListener(tag, customObjects);
+                }
+            }
+            catch (System.Exception exception) {
+                Debug.LogError("SimpleEventSystem Exception Message\n"+exception.Message);
+                Debug.LogError("Source\n" + exception.Source);
+                Debug.LogError("StackTrace\n" + exception.StackTrace);
+                Debug.LogError("InnerException\n" + exception.InnerException.Message);
+            }
+        }
+
+        public static void Dispose() {
+            CustomEventListener = null;
+
+            if (OnDisposeEvent != null)
+                OnDisposeEvent.Invoke();
+        }
+    }
+}
